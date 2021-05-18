5c44c8c5d767b9eb396a2f9ab05666bdddfcc579
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-05-18 11:16:15 +0200

    Add quiz-answers for SELECT from world tutorial

--- quiz-answers.sql
+++ quiz-answers.sql
@@ -1,3 +1,5 @@
+--SELECT Basics
+
 -- 1
 SELECT name, population
   FROM world
@@ -31,3 +33,34 @@ SELECT name, area, population
  SELECT name, population/area
   FROM world
  WHERE name IN ('China', 'Nigeria', 'France', 'Australia')
+
+-- SELECT from world
+
+-- 1.
+SELECT name
+  FROM world
+ WHERE name LIKE 'U%'
+
+-- 2.
+SELECT population
+  FROM world
+ WHERE name = 'United Kingdom'
+
+-- 3.
+'name' should be name
+
+-- 4.
+Nauru	990
+
+-- 5.
+SELECT name, population
+  FROM world
+ WHERE continent IN ('Europe', 'Asia')
+
+-- 6.
+SELECT name FROM world
+ WHERE name IN ('Cuba', 'Togo')
+
+-- 7.
+Brazil
+Colombia
