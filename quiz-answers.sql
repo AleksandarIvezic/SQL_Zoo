1d69f08ed9ee66e6dbb0e88489bcdb53ef080637
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-05-19 17:25:53 +0200

    Add quiz-answers to Using Null

--- quiz-answers.sql
+++ quiz-answers.sql
@@ -247,3 +247,28 @@ WHERE ord = 1 AND director = 351
 A Bronx Tale	1993
 Bang the Drum Slowly	1973
 Limitless	2011
+
+
+--Using Null
+
+-- 1.
+SELECT teacher.name, dept.name FROM teacher LEFT OUTER JOIN dept ON (teacher.dept = dept.id)
+-- 2.
+SELECT dept.name FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower'
+
+-- 3.
+SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name
+
+-- 4.
+display 0 in result column for all teachers without department
+
+-- 5.
+'four' for Throd
+
+-- 6.
+Shrivell	Computing
+Throd	Computing
+Splint	Computing
+Spiregrain	Other
+Cutflower	Other
+Deadyawn	Other
